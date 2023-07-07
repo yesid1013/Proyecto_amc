19f3bde3c21b77e86e14568e9d8021e84e18db40
Yesid Diaz <yesidiglesias1310@gmail.com>
2023-07-07 14:57:53 -0500

    relaciones tabla activo

--- src/models/Activo.py
+++ src/models/Activo.py
@@ -6,7 +6,7 @@ class Activo(db.Model):
     id_qr = db.Column(db.Integer, db.ForeignKey('codigos_qr.id_qr'), nullable=True)
     id_primario = db.Column(db.String(6),nullable = True)
     id_secundario = db.Column(db.String(7),nullable = True)
-    id_usuario = db.Column(db.Binary(16), db.ForeignKey('usuario_qr.id_usuario'), nullable=False)
+    id_usuario = db.Column(db.Binary(16), db.ForeignKey('usuario.id_usuario'), nullable=False)
     fecha_registro = db.Column(db.TIMESTAMP)
     ubicacion = db.Column(db.String(45),nullable = False)
     tipo_de_equipo = db.Column(db.String(55),nullable = False)
@@ -17,6 +17,9 @@ class Activo(db.Model):
     imagen_equipo = db.Column(db.String(255),nullable = True)
     id_subcliente = db.Column(db.Binary(16), db.ForeignKey('subcliente.id_subcliente'), nullable=False)
     ficha_tecnica = db.Column(db.String(255),nullable = True)
+
+    novedad = db.relationship('Novedad', backref='activo', lazy=True)
+    servicio = db.relationship('Servicio', backref='activo', lazy=True)
 
     def __init__(self,id_activo,id_qr,id_primario,id_secundario,id_usuario,ubicacion,tipo_de_equipo,fabricante,modelo,num_serie,datos_relevantes,imagen_equipo,id_subcliente,ficha_tecnica):
         self.id_activo = id_activo
