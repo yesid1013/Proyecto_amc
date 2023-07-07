a73d0ef21df2b4979b6648454c1f03cf782671ca
Yesid Diaz <yesidiglesias1310@gmail.com>
2023-07-07 15:59:19 -0500

    Modelo de tabla novedad

---
+++ src/models/Novedad.py
@@ -0,0 +1,36 @@
+from utils.db import db
+
+class Novedad(db.Model):
+    __tablename__ = 'novedad'
+    id_novedad = db.Column(db.Binary(16), primary_key=True)
+    id_activo = db.Column(db.Binary(16), db.ForeignKey('activo.id_activo'), nullable=False)
+    fecha = db.Column(db.TIMESTAMP)
+    nombre_reporta = db.Column(db.String(100),nullable=False)
+    nombre_empresa = db.Column(db.String(100),nullable=False)
+    cargo = db.Column(db.String(50),nullable=False)
+    descripcion_reporte = db.Column(db.String(100),nullable=False)
+    imagenes = db.Column(db.String(255))
+
+    def __init__(self,id_novedad,id_activo,nombre_reporta,nombre_empresa,cargo,descripcion_reporte,imagenes):
+        self.id_novedad = id_novedad
+        self.id_activo = id_activo
+        self.nombre_reporta = nombre_reporta
+        self.nombre_empresa = nombre_empresa
+        self.cargo = cargo
+        self.descripcion_reporte = descripcion_reporte
+        self.imagenes = imagenes
+    
+    def getDatos (self):
+        return {
+            "id_novedad" : self.id_novedad,
+            "id_activo" : self.id_activo,
+            "fecha" : self.fecha,
+            "nombre_reporta" : self.nombre_reporta,
+            "nombre_empresa" : self.nombre_empresa,
+            "cargo" : self.cargo,
+            "descripcion_reporte" : self.descripcion_reporte,
+            "imagenes" : self.imagenes
+        }
+
+
+
