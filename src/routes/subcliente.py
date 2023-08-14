028b424224b1c4b5e6d02278534038342958d919
Yesid Diaz <yesidiglesias1310@gmail.com>
2023-08-14 08:21:51 -0500

    Agregacion de autenticacion en la rutas de

--- src/routes/subcliente.py
+++ src/routes/subcliente.py
@@ -1,20 +1,24 @@
 from flask import Blueprint
 from flask_cors import cross_origin
 from controllers import SubclienteController
+from flask_jwt_extended import jwt_required
 
 subcliente = Blueprint('subcliente', __name__,url_prefix='/api/v1')
 
 @cross_origin()
 @subcliente.route('/subclientes', methods=['POST'])
+@jwt_required()
 def crear_subcliente():
     return SubclienteController.crear_subcliente()
 
 @cross_origin()
 @subcliente.route('/subclientes')
+@jwt_required()
 def listar_subclientes():
     return SubclienteController.listar_subclientes()
 
 @cross_origin()
 @subcliente.route('/subclientes/<id_empresa>')
+@jwt_required()
 def subclientes_de_empresa(id_empresa):
     return SubclienteController.subclientes_de_empresa(id_empresa)
