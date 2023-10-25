741ebc6b2cd063393a4c7f7a8799e9570221c3a4
Yesid Diaz <yesidiglesias1310@gmail.com>
2023-10-25 09:08:10 -0500

    Verificando si el usuario ya tiene permisos del activo escogido

--- src/controllers/PermisosController.py
+++ src/controllers/PermisosController.py
@@ -27,11 +27,20 @@ def registrar_permiso():
         id_usuario_bytes = binascii.unhexlify(id_usuario)
         id_activo_bytes = binascii.unhexlify(id_activo)
 
-        new_permiso = Permisos(id_permiso,id_usuario_bytes,id_activo_bytes,ver_informacion_basica,ver_historial_servicios,ver_novedades,registrar_servicio,registrar_novedad)
+        permiso = db.session.query(Permisos.id_permiso).filter(Permisos.id_usuario == id_usuario_bytes, Permisos.id_activo == id_activo_bytes).first() # Verificar si el usuario que tendrá los permisos ya tiene permisos del activo escogido
 
-        db.session.add(new_permiso)
-        db.session.commit()
-        return jsonify({"message": "Permiso creado correctamente", "status" : 201}), 201
+        if not permiso :
+            new_permiso = Permisos(id_permiso,id_usuario_bytes,id_activo_bytes,ver_informacion_basica,ver_historial_servicios,ver_novedades,registrar_servicio,registrar_novedad)
+
+            db.session.add(new_permiso)
+            db.session.commit()
+            return jsonify({"message": "Permiso creado correctamente", "status" : 201}), 201
+        else:
+            return jsonify({"message": "El usuario seleccionado ya tiene permisos del activo escogido", "status" : 400}), 400
+
+
+
+        
     
     except Exception as e:
         return jsonify({"message" : "Ha ocurrido un error inesperado", "error" : str(e)})
